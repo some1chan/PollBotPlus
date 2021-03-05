4c1b243cef0c2958c99261582633514d6efdfdba
some1chan <31833556+some1chan@users.noreply.github.com>
2021-03-05 01:54:16 -0600

    feat: server count for top.gg

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import { Logger } from "@framedjs/core";
 import { ShardingManager } from "discord.js";
+import AutoPoster from "topgg-autoposter";
 import path from "path";
+
+if (process.env.TOP_GG_TOKEN) {
+	const poster = AutoPoster(process.env.TOP_GG_TOKEN, ShardingManager);
+	poster.on("posted", () => {
+		// Ran when succesfully posted
+		Logger.debug("Posted stats to top.gg");
+	});
+}
 
 const manager = new ShardingManager(`${__dirname}${path.sep}Bot.js`, {
 	token: process.env.DISCORD_TOKEN,
