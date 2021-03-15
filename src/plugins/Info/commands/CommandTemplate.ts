772ebca51f13f2f5abedcde84d64b8cc7c5512e9
some1chan <31833556+some1chan@users.noreply.github.com>
2021-03-14 23:09:37 -0500

    chore: command template

---
+++ src/plugins/Info/commands/CommandTemplate.ts
@@ -0,0 +1,36 @@
+import { BaseCommand, BaseMessage, BasePlugin, Logger } from "@framedjs/core";
+
+export default class extends BaseCommand {
+	constructor(plugin: BasePlugin) {
+		super(plugin, {
+			id: "commandtemplate",
+			about: "Not a real command. Instead, you should copy me!",
+			usage: "<required param> [optional param]",
+			userPermissions: {
+				botOwnersOnly: true,
+				checkAutomatically: false,
+			},
+		});
+	}
+
+	async run(msg: BaseMessage): Promise<boolean> {
+		// Manually checks user permission to stay silent
+		const permsResult = this.checkUserPermissions(
+			msg,
+			this.userPermissions
+		);
+		if (!permsResult.success) {
+			Logger.warn(
+				`${this.id} called by non-bot user (${msg.discord?.author.id})`
+			);
+			return false;
+		}
+
+		if (msg.args) {
+			await msg.send("test");
+			return true;
+		}
+
+		return false;
+	}
+}
